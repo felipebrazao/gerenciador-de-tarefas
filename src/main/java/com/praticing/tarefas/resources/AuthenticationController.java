046a27f90800e30e0abad714e24317d376648221
Felipe Brazao <felipebrazao@outlook.com>
2025-05-14 20:52:49 -0300

    Configuração do CORS

--- src/main/java/com/praticing/tarefas/resources/AuthenticationController.java
+++ src/main/java/com/praticing/tarefas/resources/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import com.praticing.tarefas.repositories.UsuarioRepository;
 import com.praticing.tarefas.security.TokenService;
 
 import jakarta.validation.Valid;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
--- src/main/java/com/praticing/tarefas/security/SecurityConfig.java
+++ src/main/java/com/praticing/tarefas/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.praticing.tarefas.security;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,40 +18,58 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    SecurityFilter securityFilter;
+	 @Autowired
+	    SecurityFilter securityFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf(AbstractHttpConfigurer::disable) // Forma mais moderna de desabilitar CSRF
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Liberar Swagger
-                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-                .anyRequest().authenticated()
-            )
-            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
-    }
+	    @Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        return http
+	            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+	            .csrf(AbstractHttpConfigurer::disable)
+	            .sessionManagement(session -> session
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            )
+	            .authorizeHttpRequests(authorize -> authorize
+	                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+	                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+	                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+	                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+	                .anyRequest().authenticated()
+	            )
+	            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+	            .build();
+	    }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	 
+	    @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	        configuration.setAllowedHeaders(List.of("*"));
+	        configuration.setAllowCredentials(true);
+	        
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
+
+	    @Bean
+	    public AuthenticationManager authenticationManager(
+	            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	        return authenticationConfiguration.getAuthenticationManager();
+	    }
+	    
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
 }
